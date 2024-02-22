cbf7a1410873465c96282ee9137967e49218fef5
Cindy Liu <cindy.liu.667@gmail.com>
2024-02-22 16:52:28 -0500

    Add editing and delete bowel log functionalities to backend.

--- app/src/main/java/cs486/splash/model/BowelLogRepo.kt
+++ app/src/main/java/cs486/splash/model/BowelLogRepo.kt
@@ -5,13 +5,39 @@ package cs486.splash.model
  * communication to a backend server
  */
 object  BowelLogRepo {
-    var bowelLogs = mutableListOf<BowelLog>()
-    fun fetchAllBowelLogs(): List<BowelLog> {
+    // Holds bowel logs, key is the id and value is the log
+    private var bowelLogs = HashMap<String, BowelLog>()
+
+    /**
+     * Returns the bowel logs
+     */
+    fun fetchAllBowelLogs(): Map<String, BowelLog> {
         return bowelLogs
     }
 
-    fun addNewBowelLog(bowelLog: BowelLog) : List<BowelLog>{
-        bowelLogs.add(bowelLog)
+    /**
+     * Returns the bowel logs after adding [bowelLog] to the repo
+     */
+    fun addNewBowelLog(bowelLog: BowelLog) : Map<String, BowelLog>{
+        // should add logic for checking that id is unique
+        // mb also add logic for assigning ID, as that should be done in the backend as oppose to frontend
+        bowelLogs[bowelLog.id] = bowelLog
+        return bowelLogs
+    }
+
+    /**
+     * Returns the bowel logs after editing [editedBowelLog]
+     */
+    fun editBowelLog(editedBowelLog: BowelLog) : Map<String, BowelLog>{
+        bowelLogs[editedBowelLog.id] = editedBowelLog
+        return bowelLogs
+    }
+
+    /**
+     * Returns the bowel logs after deleting the bowel log with id [bowelLogId]
+     */
+    fun deleteBowelLog(bowelLogId: String) : Map<String, BowelLog> {
+        bowelLogs.remove(bowelLogId)
         return bowelLogs
     }
 }
