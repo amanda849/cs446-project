162915083133037832c378e3ae5cb92d65aad3e2
artthony <56120863+artthony@users.noreply.github.com>
2024-03-01 17:07:05 -0500

    colour picker for now, need to do this to get updates from main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
 }
 
@@ -52,4 +56,22 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Material Design 3
+    implementation("androidx.compose.material3:material3")
+    // or Material Design 2
+    //implementation("androidx.compose.material:material")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.activity:activity-compose:1.8.2")
+
 }
--- app/src/main/java/cs486/splash/ui/add/AddFragment.kt
+++ app/src/main/java/cs486/splash/ui/add/AddFragment.kt
@@ -6,9 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cs486.splash.R
 import cs486.splash.databinding.FragmentAddBinding
+import cs486.splash.viewmodels.BowelLogViewModel
 
 class AddFragment : Fragment() {
 
@@ -23,14 +42,23 @@ class AddFragment : Fragment() {
     ): View {
         val addViewModel =
             ViewModelProvider(this).get(AddViewModel::class.java)
+        val bowelLogViewModel =
+            ViewModelProvider(this).get(BowelLogViewModel::class.java)
 
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAdd
+        /*val textView: TextView = binding.textAdd
         addViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }*/
+        binding.colourPicker.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ColourPicker()
+            }
         }
+
 
         return root
     }
@@ -40,4 +68,43 @@ class AddFragment : Fragment() {
         _binding = null
     }
 
+}
+
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+@Preview
+fun ColourPicker(colour: Int = 0) {
+    val colors: List<Color> = listOf(
+        Color(0xFF9C6644),
+        Color(0xFF9C5444),
+        Color(0xFF7F5539),
+        Color(0xFF6E4428),
+        Color(0xFF744627),
+        Color(0xFF4C2206),
+    )
+    FlowRow(
+        modifier = Modifier.padding(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        for (i in colors.indices) {
+            // circle button code obtained from: https://stackoverflow.com/questions/74583523/android-jetpack-compose-how-to-create-a-button-with-an-icon-in-it
+            Button(
+                onClick = { /* ... */ },
+                shape = CircleShape,
+                modifier = Modifier.size(40.dp),
+                contentPadding = PaddingValues(1.dp),
+                colors = ButtonColors(
+                    colors[i], Color.White,
+                    colors[i], colors[i]
+                )
+            ) {
+                if (i == colour) Icon(
+                    imageVector = Icons.Default.Done,
+                    contentDescription = "Favorite",
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+        }
+    }
 }
