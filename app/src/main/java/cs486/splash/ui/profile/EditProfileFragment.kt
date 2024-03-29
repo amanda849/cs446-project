57217191f77a1028ff9bf5028b2bb46ceef63701
joannewu5 <joannewu000@gmail.com>
2024-03-29 09:56:11 -0400

    Added basic save button functionality

--- app/src/main/java/cs486/splash/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/cs486/splash/ui/profile/EditProfileFragment.kt
@@ -1,42 +1,13 @@
 package cs486.splash.ui.profile
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import cs486.splash.R
-import cs486.splash.OnboardingActivity
 import cs486.splash.databinding.FragmentEditProfileBinding
-import cs486.splash.models.UserRepository
-import cs486.splash.ui.add.PastOrPresentSelectableDates
-import cs486.splash.ui.onboarding.BirthdaySetupFragment
-import cs486.splash.ui.onboarding.SetupWelcomeFragment
 import cs486.splash.viewmodels.UserViewModel
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 class EditProfileFragment : Fragment() {
 
@@ -59,12 +30,33 @@ class EditProfileFragment : Fragment() {
             }
         }
 
+        userViewModel.userProfile.observe(viewLifecycleOwner){
+            if (it != null) {
+                _binding!!.nameField.setText(it.name)
+                _binding!!.bdayField.setText(it.birthDate)
+            }
+        }
+
+        // Save button
+        _binding!!.saveBtn.setOnClickListener {
+            // VIEW MODEL HANDLING HERE
+            // **Note: some fields (password field) are empty by default, so maybe put a check
+            //      for null string so it doesn't update the viewmodel with an empty string lol**
+
+            // nameField = user's name
+            // emailField = user's new email (default text is their original email)
+            // confirmEmailField = email confirmation
+            // passwordField = user's new password (default is empty)
+            // confirmPasswordField = password confirmation
+            // bdayField = user's birth date
+
+            // Go back to profile after updating view model
+            returnToProfile()
+        }
+
         // Back to profile page button
         _binding!!.backBtn.setOnClickListener {
-            val fragmentManager = requireActivity().supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.remove(this)
-            fragmentTransaction.commit()
+            returnToProfile()
         }
 
         return binding.root
@@ -73,5 +65,13 @@ class EditProfileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // Remove this fragment and display profile page fragment again
+    private fun returnToProfile() {
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.remove(this)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/cs486/splash/ui/profile/ProfileFragment.kt
+++ app/src/main/java/cs486/splash/ui/profile/ProfileFragment.kt
@@ -2,41 +2,17 @@ package cs486.splash.ui.profile
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import cs486.splash.R
 import cs486.splash.OnboardingActivity
 import cs486.splash.databinding.FragmentProfileBinding
 import cs486.splash.models.UserRepository
-import cs486.splash.ui.add.PastOrPresentSelectableDates
-import cs486.splash.ui.onboarding.BirthdaySetupFragment
-import cs486.splash.ui.onboarding.SetupWelcomeFragment
 import cs486.splash.viewmodels.UserViewModel
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 class ProfileFragment : Fragment() {
 
@@ -53,16 +29,16 @@ class ProfileFragment : Fragment() {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        userViewModel.user.observe(viewLifecycleOwner) {
+            if (it != null) {
+                _binding!!.userEmail.setText(it.email)
+            }
+        }
+
         userViewModel.userProfile.observe(viewLifecycleOwner){
             if (it != null) {
                 _binding!!.userName.setText(it.name)
                 _binding!!.userBirthdate.setText(it.birthDate)
-            }
-        }
-
-        userViewModel.user.observe(viewLifecycleOwner) {
-            if (it != null) {
-                _binding!!.userEmail.setText(it.email)
             }
         }
 
