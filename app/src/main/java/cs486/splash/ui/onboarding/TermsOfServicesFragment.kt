d8caf44cc9537ea09af69dee3f39bf8d231d9aca
Cindy Liu <kxliu@uwaterloo.ca>
2024-04-01 00:04:07 -0400

    Made onboarding a bit prettier

--- app/src/main/java/cs486/splash/ui/onboarding/TermsOfServicesFragment.kt
+++ app/src/main/java/cs486/splash/ui/onboarding/TermsOfServicesFragment.kt
@@ -1,12 +1,14 @@
 package cs486.splash.ui.onboarding
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import cs486.splash.R
+import androidx.fragment.app.Fragment
 import cs486.splash.databinding.FragmentTermsOfServicesBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,21 +30,22 @@ class TermsOfServicesFragment : Fragment() {
     ): View {
         _binding = FragmentTermsOfServicesBinding.inflate(inflater, container, false)
 
+        val termsAndServicesContent = getString(cs486.splash.R.string.terms_and_services_content)
+        _binding!!.termsAndSevices.text = Html.fromHtml(termsAndServicesContent)
+
         _binding!!.agreeButton.setOnClickListener {
             val fragmentManager = requireActivity().supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragmentContainerView, SignUpFragment())
+            fragmentTransaction.replace(cs486.splash.R.id.fragmentContainerView, SignUpFragment())
             fragmentTransaction.addToBackStack(null) // Optional: Add to back stack
             fragmentTransaction.commit()
         }
 
         _binding!!.disagreeButton.setOnClickListener {
-            // TODO: Show warning
-
             // change to welcome fragment
             val fragmentManager = requireActivity().supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragmentContainerView, WelcomeFragment())
+            fragmentTransaction.replace(cs486.splash.R.id.fragmentContainerView, WelcomeFragment())
             fragmentTransaction.addToBackStack(null) // Optional: Add to back stack
             fragmentTransaction.commit()
         }
